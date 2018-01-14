3da630baf7cf3a5f166250f0984ba487525a94a5
Asim1996 <awesomeasim2596@gmail.com>
2018-01-14 17:14:38 +0530

    Add Ajax functionality

--- index-express.js
+++ index-express.js
@@ -1,57 +1,13 @@
 const express=require('express');
 var bodyParser=require('body-parser');
-const request=require('request');
-
-const axios=require('axios');
-// const config=require('./config.js');
-// var API=config.DarkskyApiKEY;
-
-const hbs=require('hbs');
-const port=process.env.PORT||3000;
 var app=express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine','hbs');
+app.use(express.static(__dirname + "/public"));
+const port=process.env.PORT||3000;
+
 
 app.get('/',(req,res)=>{
-	res.render('home.hbs');
+	res.render('index');
 })
-app.get("/result",function(req,res){
-    var address=req.query.address;
-    var lat,lng;
-    console.log(address);
-	var encodedAddress=encodeURIComponent(address);
-var geocodeUrl=`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
-axios.get(geocodeUrl).then((response)=>{
-	if(response.data.status==='ZERO_RESULTS'){
-		throw new Error('Unable to find that address')
-	}
-	var lat=response.data.results[0].geometry.location.lat;
-	var lng=response.data.results[0].geometry.location.lng;
-	
-	var weatherUrl=`https://api.darksky.net/forecast/6b62075a7ba9b8d3dc274c35d1bd832c/${lat},${lng}`;
-		// var weatherUrl=`https://api.darksky.net/forecast/${API}/${lat},${lng}`;
-	// res.render("result",{
-	// 	address:response.data.results[0].formatted_address,
-	// 	lat:response.data.results[0].geometry.location.lat,
-	// 	lng:response.data.results[0].geometry.location.lng})
-	return axios.get(weatherUrl);
-}).then((response)=>{
-	var temperature=response.data.currently.temperature;
-	var apparentTemperature=response.data.currently.apparentTemperature;
-	res.render("result",{
-		temperature:temperature,
-		apparentTemperature:apparentTemperature
-		});
-}).catch((e)=>{
-	if(e.code==='ENOTFOUND'){
-		console.log('Unable to connect to API servers');
-	}else{
-		console.log(e.message);
-	}
-});
- }
- ) 
-
 app.listen(port,()=>{
 	console.log(`Server is up on port ${port}`);
 });
--- public/scriptfile/script.js
+++ public/scriptfile/script.js
@@ -1,27 +1,24 @@
-function loadData() {
-var address=$('#address').val();
+jQuery('#form-container').on('submit',function(e){
+  e.preventDefault();  
+  var address=$('#address').val();
 var geocodeUrl='https://maps.googleapis.com/maps/api/geocode/json?address='+address+'';
-    
+
     $("#formaddress").html("");
     $("#temp").html("");
     $("#err").html("");
 
-
- $.ajax({
+$.ajax({
         type:"GET",
-        url:geocodeUrl,
+        url:geocodeUrl
     }).done(function(data){
             console.log(data);
-             
-
-             	if(data.status==='ZERO_RESULTS'){
-					$('#err').html('Unable to find that address')
-				}
+                if(data.status==='ZERO_RESULTS'){
+                    $('#err').text('Unable to find that address')
+                }
              else{
             var address=data.results[0].formatted_address;
             var lat=data.results[0].geometry.location.lat;
             var lng=data.results[0].geometry.location.lng;
-
             $('#formaddress').html(address);
             
              var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+lat+","+lng+'';
@@ -29,42 +26,35 @@ var geocodeUrl='https://maps.googleapis.com/maps/api/geocode/json?address='+addr
              
             $.ajax({
                 type:"GET",
-                url:metaUrl,
+                url:metaUrl
             }).done(function(data){
                 console.log(data);
-                if(data.status==='OK'){
-                    $('body').append('<img class="bgimg" src="' + streetviewUrl + '">');
-                }else{
+                if(data.status==='ZERO_RESULTS'){
                     $('body').css('background-color','#232323');
-                }
+                }else{
+                $('body').append('<img class="bgimg" src="' + streetviewUrl + '">');
+                   }
             })
             
              var forecastRequestTimeout = setTimeout(function(){
-                
+              
                 }, 8000);
 
             $.ajax({
                 type: "get",
                 url: "https://api.darksky.net/forecast/6b62075a7ba9b8d3dc274c35d1bd832c/"+lat+","+lng+'',
-                dataType: "jsonp",
+                dataType: "jsonp"
              }).done(function (data) {
                     console.log('Temperature:',data.currently.temperature);
                     var temp=Math.round((data.currently.temperature-32)/1.8);
                     $('#temp').html('Temperature:'+temp+'&deg;C '+data.currently.summary+'<br><br>'+'Summary:'+data.hourly.summary);
                 clearTimeout(forecastRequestTimeout);
-                })
-          }   
-            
-            
-        }).catch((e)=>{
-        	console.log(e);
-        })
-    
-
-
-return false;
-}
- $('#form-container').submit(loadData);
+                }).catch((e)=>{
+         console.log(e);
+        })            
+ }
+})
+})
 
 
 
