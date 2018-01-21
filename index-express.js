679d4c6bcd30078db38f6af3abda2cbe6602b49f
Asim1996 <awesomeasim2596@gmail.com>
2018-01-21 21:37:41 +0530

    hbs to ejs and add result page

--- index-express.js
+++ index-express.js
@@ -1,31 +1,26 @@
 const express=require('express');
-const hbs=require('hbs');
 const axios=require('axios');
 const config=require('./config.js');
 var API=config.DarkskyApiKEY;
-
 var bodyParser=require('body-parser');
 var app=express();
 app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine','hbs');
-app.use(express.static(__dirname + "/public"));
-hbs.registerPartials(__dirname+'/views/partials')
-
+app.set("view engine","ejs");
+app.use(express.static('public'))
 const port=process.env.PORT||3000;
-
-
 app.get('/',(req,res)=>{
-	res.render('index.hbs');
+	res.render('index.ejs');
 })
 app.post('/result',async(req,res)=>{
 	var address=req.body.location;
+	try{
 	var encodedAddress=encodeURIComponent(address);
-	var geocodeUrl=`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+	var geocodeUrl=`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyAoPo7AVWoeYECEWDsGCI1KHwP5qk5d7a8`;
 	console.log(geocodeUrl);
-	var response=await axios.get(geocodeUrl);
+	
+		var response=await axios.get(geocodeUrl);
 	if(response.data.status==='ZERO_RESULTS'){
-		// throw new Error('Unable to find that address')
-		var error="Unable to find that address";
+			throw new Error('Unable to find that address')
 	}else{
 	var lat=response.data.results[0].geometry.location.lat;
 	var lng=response.data.results[0].geometry.location.lng;
@@ -36,17 +31,28 @@ app.post('/result',async(req,res)=>{
 	var temperature=weatherResponse.data.currently.temperature;
 	var	apparentTemperature=weatherResponse.data.currently.apparentTemperature;
 	var weatherSummary=weatherResponse.data.currently.summary;
+	var humidity=weatherResponse.data.currently.humidity;
+	var visibility=weatherResponse.data.currently.visibility;
 	console.log(temperature,apparentTemperature,weatherSummary);
 	console.log(lat,lng,formattedaddress);	
 	
-	res.render('result.hbs',{
+	res.render('result.ejs',{
 		formattedaddress:formattedaddress,
+		lat:lat,
+		lng:lng,
 		temperature:temperature,
 		apparentTemperature:apparentTemperature,
 		weatherSummary:weatherSummary,
-		
+		humidity:humidity,
+		visibility:visibility
+
 		});
+	}	
+	}catch(e){
+		alert('Unable to get that address')
+		res.redirect('/');
 	}
+	
 })
 
 app.listen(port,()=>{
