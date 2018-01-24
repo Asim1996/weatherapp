fd44f4bae8ca818e80726603f19921a0a184f781
Asim1996 <awesomeasim2596@gmail.com>
2018-01-24 22:05:12 +0530

    Update UX

--- public/scriptfile/script.js
+++ public/scriptfile/script.js
@@ -1,13 +1,30 @@
 $(function() {
-
 $("#address").focus(function(){
 $(".close-btn").addClass("active");
 });
-
 $("#address").focusout(function(){
   $(".close-btn").removeClass("active");
 });
 });
+function showLoader() {
+  var overlay = document.getElementById("overlay");
+  var mainContent = document.getElementById("mainContent");
+  overlay.style.display = 'block';
+  mainContent.style.display = 'none';
+  setTimeout( function(){
+    overlay.style.display = 'none';
+    mainContent.style.display = 'block';
+  }, 15000);
+}
+
+var overlay = document.getElementById("overlay");
+var mainContent = document.getElementById("mainContent");
+mainContent.style.display = 'none';
+window.addEventListener('load', function(){
+  overlay.style.display = 'none';
+  mainContent.style.display = 'block';
+  
+})
 
 
 
